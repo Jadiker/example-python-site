805bb71f6764324d7cbda0ed4d09efd05cb482c4
Jadiker <happylondon4@gmail.com>
2020-04-07 17:49:10 -0500

    site running

--- static/js/schedule.jsx
+++ static/js/schedule.jsx
@@ -1,7 +1,6 @@
 // index.jsx
 import React from "react";
 import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.css';
 import SubmitTextBox from './components/submitTextBox.jsx';
 
 // TODO edit this
